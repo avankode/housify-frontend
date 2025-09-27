60c4628138e5c4e6ab9c4649f805adce07badef0
avankode <avaneesh.kul@gmail.com>
2025-09-27 13:25:45 +0530

    all functionailities but all in one page

---
+++ app/util.ts
@@ -0,0 +1,19 @@
+export interface User {
+    username: string;
+    email: string;
+}
+
+export const getCookie = (name: string) => {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
